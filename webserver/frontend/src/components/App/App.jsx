156b32ffc889d0116d1ee83ac58ff69e81769679
ElijahInamarga <elijahcek@gmail.com>
2025-11-08 20:14:15 -0800

    Initital banner draft

--- webserver/frontend/src/components/App/App.jsx
+++ webserver/frontend/src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from '../Header/Header.jsx'
+import Banner from '../Banner/Banner.jsx'
 
 function App() {
 
   return (
     <>
       <Header/>
+      <Banner/>
     </>
   )
 }
---
+++ webserver/frontend/src/components/Banner/Banner.jsx
@@ -0,0 +1,5 @@
+import './Banner.css'
+
+export default function Banner() {
+    return <img src="../../../public/banner-love-library.jpg" alt="banner" width="100%" height="100%" />;
+}
