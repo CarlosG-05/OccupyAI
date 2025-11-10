0a9a1f3d10cc43e4f5ce2fdd35f7d73be6a0fafd
ElijahInamarga <elijahcek@gmail.com>
2025-11-09 16:12:46 -0800

    Latest Header implementation

--- webserver/frontend/src/components/Header/Header.jsx
+++ webserver/frontend/src/components/Header/Header.jsx
@@ -6,11 +6,25 @@ export default function Header() {
       <div class="container">
         <img class="title" src="/logo.png" alt="logo" width="200px" height="200px"/> 
         <h1>Love Library Study Rooms</h1>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
+
+        <div class="dropdown">
+            <button class="dropbtn">Floor Level ᐯ</button>
+            <div class="dropdown-content">
+                <a href="#">Floor 1</a>
+                <a href="#">Floor 2</a>
+                <a href="#">Floor 3</a>
+                <a href="#">Floor 4</a>
+                <a href="#">Floor 5</a>
+            </div>
+        </div>
+
+        <div class="dropdown" id="more">
+            <button class="dropbtn">☰</button>
+            <div class="dropdown-content">
+                <a href="#">Contact</a>
+                <a href="#">About</a>
+            </div>
+        </div>
       </div>
     </>
   )
